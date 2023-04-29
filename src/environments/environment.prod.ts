729e004039739c5217395f181f76d68524c11498
fontanaUTN <fontana.utn@gmail.com>
2023-04-29 18:49:58 -0300

    edit env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,6 @@
 export const environment = {
-  firebase: {
-    projectId: 'portfolio-frontend-cc3de',
-    appId: '1:27694538060:web:2af7e0e229fef77a1603e9',
-    storageBucket: 'portfolio-frontend-cc3de.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyAtEww2AM73_iXvLDHB5rlay1eDfcMEFC4',
-    authDomain: 'portfolio-frontend-cc3de.firebaseapp.com',
-    messagingSenderId: '27694538060',
-    measurementId: 'G-F18JFWVGYY',
-  },
   production: false,
-  URL: 'http://localhost:8080/'
+  // URL: 'http://localhost:8080/'
   // URL: 'https://portfoliobackend-h27w.onrender.com/'
-  // URL: 'https://fontana.progresodigital.net/'
+  URL: 'https://fontana.progresodigital.net/'
 };
